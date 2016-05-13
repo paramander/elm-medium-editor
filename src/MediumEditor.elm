9f7381919449be10c596ff0f67bb4894c0797122
Tolga Paksoy <tolga@paramander.com>
2016-05-14 01:33:57 +0200

    Pass the initial content as a parameter to init

--- src/MediumEditor.elm
+++ src/MediumEditor.elm
@@ -18,29 +18,51 @@ import MediumEditor.Ports exposing (PortEditorOptions, defaultOptions, initMediu
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
-{-| test -}
+{-| Define the editor with its own id, or class. You have the freedom to either
+use the id, or the class when creating the editor via ports. The contents are stored
+as a `String`.
+
+    init "my-editor-id-1" "editable-class"
+
+-}
 type alias Editor
   = { id : String
     , class : String
     , content : String
     }
 
-{-| test -}
+{-| The possible mutations this `Editor` can handle are defined in `Msg`. When using
+elm-medium-editor, you are meant to wrap this `Msg` into your own `Msg` constructor.
+After wrapping the `MediumEditor.Msg` type, you can use `Html.map` to communicate
+state changes to your `Editor`.
+
+    type Msg
+      = NoOp
+      | EditorMsg MediumEditor.Msg
+
+-}
 type Msg
   = NoOp
   | UpdateContent String
 
-{-| test -}
-init : String -> String -> (Editor, Cmd Msg)
-init id class =
+{-| To initialize a new editor, use the init function. It takes an id, class and
+the initial HTML content as a String. It is strongly advised you use this init
+method and not the Record constructor.
+
+Setting up an editor is an effect. It will call the initialization port for you.
+In this initialization port, the editor is setup given a set of options defined
+in the `Ports` module.
+-}
+init : String -> String -> String -> (Editor, Cmd Msg)
+init id class content =
   let
     editor =
       { id = id
       , class = class
-      , content = "<p>My father's family name being <a href=\"https://en.wikipedia.org/wiki/Pip_(Great_Expectations)\">Pirrip</a>, and my Christian name Philip, my infant tongue could make of both names nothing longer or more explicit than Pip. So, I called myself Pip, and came to be called Pip.</p><p>I give Pirrip as my father’s family name, on the authority of his tombstone and my sister,—Mrs. Joe Gargery, who married the blacksmith. As I never saw my father or my mother, and never saw any likeness of either of them (for their days were long before the days of photographs), my first fancies regarding what they were like were unreasonably derived from their tombstones. The shape of the letters on my father’s, gave me an odd idea that he was a square, stout, dark man, with curly black hair. From the character and turn of the inscription, “Also Georgiana Wife of the Above,” I drew a childish conclusion that my mother was freckled and sickly. To five little stone lozenges, each about a foot and a half long, which were arranged in a neat row beside their grave, and were sacred to the memory of five little brothers of mine,—who gave up trying to get a living, exceedingly early in that universal struggle,—I am indebted for a belief I religiously entertained that they had all been born on their backs with their hands in their trousers-pockets, and had never taken them out in this state of existence.</p>"
+      , content = content
       }
     options =
-      withContent editor.content <| defaultOptions id
+      withContent content <| defaultOptions id
   in
     ( editor
     , initMediumEditor options
